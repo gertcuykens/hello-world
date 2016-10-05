a7b4b9b226d83b96bfdd3563df6f496f2dd7b325
Gert Cuykens <gert.cuykens@gmail.com>
2016-10-05 21:12:49 +0200

    ts

--- src/hello-world-es.ts
+++ src/hello-world-es.ts
@@ -2,10 +2,10 @@ class HelloWorldES extends HTMLElement {
   _hello:string = "Hello World"                                                                                      
   constructor() {                                                                                                            
     super()
-    const l: HTMLLinkElement = document.querySelector('link[rel="import"]')                                                                     
-    if(l == null) {return}
-    const t = l.import.querySelector('template') 
-    if(t == null) {return}                                                                           
+    const l = document.querySelector('link[rel="import"]') as HTMLLinkElement                                                                    
+    if (l == null) return;
+    const t = l.import.querySelector('template')
+    if (t == null) return;                                                                          
     const instance = t.content.cloneNode(true)                                                                      
     const shadowRoot = this.attachShadow({ mode: 'open' })                                                                     
     shadowRoot.appendChild(instance)                                                                                         
